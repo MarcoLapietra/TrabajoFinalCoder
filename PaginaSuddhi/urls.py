2cf8341a9670b211ce43a168ed2f33083c2b54c2
Marco <marcolucaslapietra@gmail.com>
2023-09-24 18:32:26 -0300

    se agregaron las views de cursos, productos, tinturas y cuidado corporal. Tambien se dividio en sectores el navbar y el template padre

--- PaginaSuddhi/urls.py
+++ PaginaSuddhi/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', inicio, name="Inicio"),
+    path('cuidado_corporal', cuidado_corporal, name="CuidadoCorporal"),
+    path('tinturas', tinturas_madre, name="Tinturas"),
+    path('cursos', cursos, name="Cursos"),
 ]
--- PaginaSuddhi/views.py
+++ PaginaSuddhi/views.py
@@ -8,3 +8,15 @@ from django.http import HttpResponse
 
 def inicio(req):
     return render(req, "inicio.html") 
+
+
+
+def cuidado_corporal(req):
+    return render (req, "cuidado_corporal.html")
+
+
+def tinturas_madre(req):
+    return render(req, "tinturas.html")
+
+def cursos(req):
+    return render(req, "cursos.html")
